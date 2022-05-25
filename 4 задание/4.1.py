478afaa0624e93e8fecd69c08e714bca0a7e81bd
MichaelZacarniy <100606624+MichaelZacnarris@users.noreply.github.com>
2022-05-25 23:11:00 +0500

    Update 4.1.py
    PIP

--- 4 задание/4.1.py
+++ 4 задание/4.1.py
@@ -23,3 +23,17 @@ class FoodInfo:
     def __repr__(self):
         return 'FoodInfo({},{},{},{})'.format(self.get_proteins(), self.get_fats(), self.get_carbohydrates(),
                                               self.get_kcalories())
+    def __add__(self, addition):
+        return FoodInfo(self.get_proteins() + addition.get_proteins(), self.get_fats() + addition.get_fats(),
+                        self.get_carbohydrates() + addition.get_carbohydrates())
+  
+
+food1 = FoodInfo(100, 100, 100)
+food2 = FoodInfo(50, 60, 70)
+food3 = food1 + food2
+print(food1.get_proteins(), food1.get_fats(),
+      food1.get_carbohydrates(), food1.get_kcalories())
+print(food2.get_proteins(), food2.get_fats(),
+      food2.get_carbohydrates(), food2.get_kcalories())
+print(food3.get_proteins(), food3.get_fats(),
+      food3.get_carbohydrates(), food3.get_kcalories())
