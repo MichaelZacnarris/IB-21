83a8a68e18a52edd83c63ff5cd41036985f517ee
MichaelZacarniy <100606624+MichaelZacnarris@users.noreply.github.com>
2022-05-25 22:58:29 +0500

    Update 4.1.py

--- 4 задание/4.1.py
+++ 4 задание/4.1.py
@@ -1,1 +1,11 @@
-а
+class FoodInfo:
+    def __init__(self, proteins, fats, carbohydrates):
+        self.proteins = proteins
+        self.fats = fats
+        self.carbohydrates = carbohydrates
+    def get_proteins(self):
+        return self.proteins
+    def get_fats(self):
+        return self.fats
+    def get_carbohydrates(self):
+        return self.carbohydrates
